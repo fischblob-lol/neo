daff612a6f1e6e3312e5105f1e64e7e9e136fe62
Daniel <235312011+fischblob-lol@users.noreply.github.com>
2026-04-05 20:29:10 +0200

    finish installer

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-// Created by Daniel on April 5th
+# Created by Daniel on April 5th
+set e
 echo "####################"
 echo "# NeoPkg Installer #"
 echo "####################"
@@ -6,4 +7,12 @@ echo "Install will start in 5 seconds. CTRL + C to abort."
 sleep 5
 echo "Starting install."
 echo "--- STEP 1: Get the NeoPkg bin ---"
-curl -o https://raw.githubusercontent.com/sigmaboyihate/neo/zig-out/
+curl -o neo https://raw.githubusercontent.com/sigmaboyihate/neo/zig-out/bin/neo
+echo "--- STEP 2: Create Directory and move NeoPkg bin to /opt/NeoPkg/bin"
+mkdir -p /opt/NeoPkg/bin
+mv neo /opt/NeoPkg/bin/neo
+chmod +x /opt/NeoPkg/bin/neo
+echo "--- Installation is done, wooho! ---
+echo "!!! Next steps: !!!"
+echo "Add the NeoPkg bin to your $PATH using this command:"
+echo "export PATH="/opt/NeoPkg/bin:$PATH"
