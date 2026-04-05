030fb4253ded275df46aebd65d7758f40e614540
Daniel <235312011+fischblob-lol@users.noreply.github.com>
2026-04-05 22:54:52 +0200

    dont propose this plz

--- build.zig
+++ build.zig
@@ -12,7 +12,6 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/main.zig"),
             .target = b.graph.host,
             // .strip = true,
-            .optimize = .Debug,
         }),
     });
 
