bd56eee4ad8d2457f83306a872c084eb0caa811a
Daniel <235312011+fischblob-lol@users.noreply.github.com>
2026-04-05 20:23:36 +0200

    im not done yet this is just to save my stuff

---
+++ install.sh
@@ -0,0 +1,9 @@
+// Created by Daniel on April 5th
+echo "####################"
+echo "# NeoPkg Installer #"
+echo "####################"
+echo "Install will start in 5 seconds. CTRL + C to abort."
+sleep 5
+echo "Starting install."
+echo "--- STEP 1: Get the NeoPkg bin ---"
+curl -o https://raw.githubusercontent.com/sigmaboyihate/neo/zig-out/
